ea9d250d37e1a97fd847570f38d0897d14c3901e
Thosam1 <thosamtenzin2002@gmail.com>
2022-10-09 20:28:21 +0200

    everything else

---
+++ demo-one/src/com/luv2code/springdemo/Coach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+// interface only declare methods
+public interface Coach {
+
+    public String getDailyWorkout();
+
+    public String getDailyFortune();
+
+
+}
