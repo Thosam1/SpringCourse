5a22cb4172365bbe209831d4d2d314cfd708bd9b
Thosam1 <thosamtenzin2002@gmail.com>
2022-10-09 20:43:24 +0200

    setter injection done

--- demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -1,4 +1,27 @@
 package com.luv2code.springdemo;
 
-public class CricketCoach {
+public class CricketCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    // create a no-arg constructor
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    // our setter method (called by Spring to inject the dependency)
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
 }
