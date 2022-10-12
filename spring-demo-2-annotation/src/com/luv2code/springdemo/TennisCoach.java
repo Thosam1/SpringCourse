abbdfcdcaefe46b1caafca0bafdc48f5d0cd9352
Thosam1 <thosamtenzin2002@gmail.com>
2022-10-12 22:09:58 +0200

    Injection @Autowired with @Qualifier

--- spring-demo-2-annotation/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-2-annotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component // Spring will automatically register this components
 public class TennisCoach implements Coach {
 
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -13,20 +16,22 @@ public class TennisCoach implements Coach {
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
-    // define a setter method
-    @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside setFortuneService() method");
-        this.fortuneService = fortuneService;
-    }
+//    // define a setter method
+//    @Autowired
+//    public void setFortuneService(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside setFortuneService() method");
+//        this.fortuneService = fortuneService;
+//    }
 
-    // define a setter method
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    // for setter injection
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
+    // // for constructor injection
+//    // define a setter method
 //    @Autowired
 //    public TennisCoach(FortuneService fortuneService) {
 //        this.fortuneService = fortuneService;
